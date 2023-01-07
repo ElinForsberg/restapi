96bf038d8b7bbf1a3e29e22ecc9ed83f5ee9713d
Elin Forsberg <elinfsp96@hotmail.com>
2023-01-07 08:31:33 +0700

    slut fix

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ app.use(express.json())
 
 
 app.get('/objects', function(req,res){
-
+  
    fs.readFile("objekt.json", function(err,data){
+    if(err){
+      console.log(err);
+      res.status(404).send("Filen kan inte hittas!")
+    }
     const objekt = JSON.parse(data)
     res.send(objekt)
 }) 
@@ -20,35 +24,31 @@ app.get('/objects', function(req,res){
 
 app.post('/objects', function(req,res)  {
    
-   
-// Storing the JSON format data in myObject
-var data = fs.readFileSync("objekt.json");
-var myObject = JSON.parse(data);
+const data = fs.readFileSync("objekt.json");
+const myObject = JSON.parse(data);
   
-// Defining new data to be added
 let newData = req.body;
   
-// Adding the new data to our object
 myObject.push(newData);
-  
-// Writing to our JSON file
-//var newData2 = JSON.stringify(myObject, null, 2);
+
 fs.writeFile("objekt.json", JSON.stringify(myObject,null,2), (err) => {
-  // Error checking
-  //if (err) throw err;
+  if(err){
+    console.log(err);
+    res.status(404).send("Objektet gick inte att lägga till!")
+  }
   console.log("New data added");
 });
        
     res.status(201).json(req.body)
-   
-   //res.status(201).send(myObject)
 })
 
+
 app.get('/objects/byid/:id', (req,res) =>{
+
   fs.readFile("objekt.json", function(err,data){
     if(err){
       console.log(err);
-      res.status(404).send("filen finns inte")
+      res.status(404).send("Filen kan inte hittas!")
     }
     let myObject = JSON.parse(data)
     const { id } = req.params;
@@ -60,51 +60,51 @@ app.get('/objects/byid/:id', (req,res) =>{
 });
 
 app.put('/objects/:id', function(req,res){
+
   fs.readFile("objekt.json", function(err,data){
   if(err){
     console.log(err);
-    res.status(404).send("filen finns inte")
+    res.status(404).send("Objektet hittas inte!")
   }
     let myObject = JSON.parse(data);
     const {id}= req.params;
    
     
     const { type, location, bedrooms, price} = req.body;
-    // let newData = req.body;
-    // myObject.push(newData);
+    
     const findObject = myObject.find((object) => object.id ===id);
-   if(!myObject) res.status(404).send("det gick inte att uppdatera");
+   if(!myObject) res.status(404).send("Objektet gick ite att ändra!");
     if(type) findObject.type = type;
     if(location) findObject.location = location;
     if(bedrooms) findObject.bedrooms = bedrooms;
     if(price) findObject.price = price;
 
-// Writing to our JSON file
-//var newData2 = JSON.stringify(myObject, null, 2);
+
 fs.writeFile("objekt.json", JSON.stringify(myObject,null,2), (err) => {
-  // Error checking
-  if (err) throw err;
-  console.log("data changed");
+  if(err){
+    console.log(err);
+  }
+  
 });
-       
+    console.log("data changed");
     res.status(200).send(myObject)
    
-    //res.send()
 });
 });
 
 app.delete('/objects/:id', (req, res) => {
+  
   fs.readFile("objekt.json", function (err, data){
     if(err) {
       console.log(err);
-      res.status(404).send("objektet finns inte")
+      res.status(404).send("objektet hittas inte!")
     }
     let myObjects = JSON.parse(data)
     const { id } = req.params;
     const objects = myObjects.find((object) => object.id === id);
 
     if(!objects){
-    res.status(404).send("objektet kan inte tas bort");
+    res.status(404).send("Objektet gick inte att radera!");
     } else {
       myObjects = myObjects.filter((object) => object.id !== id);
       fs.writeFile("objekt.json",JSON.stringify(myObjects, null, 2), function(err){
@@ -112,13 +112,11 @@ app.delete('/objects/:id', (req, res) => {
           console.log(err);
         }
       });
-      res.send("raderad");
+      res.send(myObjects);
       
     }
   });
 });
-
-
   
 app.listen(port, () => console.log("servern är igång"))
 
